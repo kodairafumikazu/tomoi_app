210e15e2f13975325153519c11e7fe25c02a12cf
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-03-05 21:52:33 +0900

    addressesモデルとテーブル作成

--- db/migrate/20210223072228_create_products.rb
+++ db/migrate/20210223072228_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string     :product_name, null: false
-      t.integer    :price, null: false
-      t.text       :info, null: false
+      t.string     :product_name,  null: false
+      t.integer    :price,         null: false
+      t.text       :info,          null: false
       t.timestamps
     end
   end
--- db/migrate/20210223082146_create_order_items.rb
+++ db/migrate/20210223082146_create_order_items.rb
@@ -1,9 +1,9 @@
 class CreateOrderItems < ActiveRecord::Migration[6.0]
   def change
     create_table :order_items do |t|
-      t.integer    :quantity, null: false
-      t.references :product, foreign_key: true
-      t.references :order, foreign_key: true
+      t.integer    :quantity,      null: false
+      t.references :product,       foreign_key: true
+      t.references :order,         foreign_key: true
       t.timestamps
     end
   end
