591daf831593ab9d3f2baac692d56273e66ac73f
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-03-25 12:58:10 +0900

    application-helper.rb内記述の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,33 +1,33 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'サイト名',
-      title: 'タイトル',
+      site: 'トモイ珈琲',
+      title: 'トモイ珈琲 online shop',
       reverse: true,
       separator: '|',
-      description: 'ディスクリプション',
-      keywords: 'キーワード',
+      description: '自家農園から採れた厳選した珈琲豆を使用。コクと深みのある味を実現する為製法にこだわっています。',
+      keywords: '珈琲',
       canonical: request.original_url,
       noindex: ! Rails.env.production?,
       icon: [
-        { href: image_url('favicon.ico') },
-        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+        { href: image_url('751280.jpg') },
+        { href: image_url('251280.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
       ],
       og: {
-        site_name: 'サイト名',
-        title: 'タイトル',
-        description: 'ディスクリプション', 
+        site_name: 'トモイ珈琲',
+        title: 'トモイ珈琲 online shop',
+        description: '自家農園から採れた厳選した珈琲豆を使用。コクと深みのある味を実現する為製法にこだわっています。', 
         type: 'website',
         url: request.original_url,
-        image: image_url('ogp.png'),
+        image: image_url('1219729.jpg'),
         locale: 'ja_JP',
       },
       twitter: {
         card: 'summary_large_image',
-        site: '@ツイッターのアカウント名',
+        site: '@Fumi_Kazu5',
       }
       fb: {
-        app_id: '自身のfacebookのapplication ID'
+        app_id: 'fumikazu.kodaira.5'
       }
     }
   end
