e1723a140915895405ca06064b2ad9b82c0166c9
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-03-25 12:28:38 +0900

    gem 'meta-tags'とapplication-helper.rb内記述

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'サイト名',
+      title: 'タイトル',
+      reverse: true,
+      separator: '|',
+      description: 'ディスクリプション',
+      keywords: 'キーワード',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'サイト名',
+        title: 'タイトル',
+        description: 'ディスクリプション', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@ツイッターのアカウント名',
+      }
+      fb: {
+        app_id: '自身のfacebookのapplication ID'
+      }
+    }
+  end
 end
